8453a43c4301c0e561d45ce1c0dd4db3d001ff92
Boguslav Semenov <semenov0310@gmail.com>
2020-09-24 14:40:31 +0300

    # Conflicts: #	src/user/routes.py

--- src/user/routes.py
+++ src/user/routes.py
@@ -10,44 +10,49 @@ from src.user.utils import authorized, current_user
 user = Blueprint("user_bp", url_prefix="/user")
 
 
-@user.post('/login')
+@user.route('/login', methods=['POST', 'OPTIONS'])
 async def auth(request):
     if request.cookies.get('session') in sessions:
         return redirect('/')
 
-    if request.args.get("username") and request.args.get("password"):
-        username = request.args.get("username")
-        password = hashlib.sha3_512(
-            bytes(request.args.get("password"), encoding='utf8')).hexdigest()
-        user = await User.query.where(
-            (User.username == username)
-            & (User.password == password)
-        ).gino.first()
+	if request.args.get("username") and request.args.get("password"):
+		username = request.args.get("username")
+		password = hashlib.sha3_512(bytes(request.args.get("password"), encoding='utf8')).hexdigest()
+		user = await User.query.where(
+			(User.username == username)
+			& (User.password == password)
+			).gino.first()
 
-        if user:
-            hs = secrets.token_hex(nbytes=16)
-            response = json({'token': user.username})
-            response.cookies['session'] = hs
-            response.cookies['session']['secure'] = True
-            response.cookies['session']['httponly'] = True
-            response.cookies['session']['max-age'] = 60 * 60 * 24 * 30
-            sessions[hs] = user
-        else:
-            response = json({'error': "No user"})
-    else:
-        response = json({'error': "No args"})
+		if user:
+			hs = secrets.token_hex(nbytes=16)
+			response = json({'token': user.username})
+			response.cookies['session'] = hs
+			response.cookies['session']['httponly'] = True
+			response.cookies['session']['max-age'] = 60 * 60 * 24 * 30
+			sessions[hs] = user
+		else:
+			response = json({'error': "No user"})
+	else:
+		response = json({'error': "No args"})
 
-    return response
+	if request.method == 'OPTIONS':
+		response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5000'
+		response.headers['Access-Control-Allow-Methods'] = 'POST'
+		response.headers['Access-Control-Allow-Credentials'] = 'true'
+	if request.method == 'POST':
+		response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5000'
+		response.headers['Access-Control-Allow-Credentials'] = 'true'
+
+	return response
 
 
 @user.get("/logout")
 @authorized()
 async def logout(request):
-    response = redirect("/")
-    del sessions[request.cookies.get('session')]
-    del response.cookies['session']
-    return response
-
+	response = redirect("/")
+	del sessions[request.cookies.get('session')]
+	del response.cookies['session']
+	return response
 
 @user.get("/<user_id:int>")
 @user.get("/")
